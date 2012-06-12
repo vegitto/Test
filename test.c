e0df4a623724d1fe3fc98c590a4b49538cb55df4
Stan Ioan <stanioanmihail@gmail.com>
2012-06-12 12:35:00 +0300

    min max

--- test.c
+++ test.c
@@ -7,11 +7,21 @@ void citire(int *x,int *y){
 void afisare(int x,int y){
 	printf("%d %d\n",x,y);
 }
+int get_min(int x, int y)
+{
+	return x < y ? x : y;
+}
+int get_max(int x, int y)
+{
+	return x > y ? x : y; 
+}
 
 int main()
 {
 	int x,y;
 	citire(&x,&y);
 	afisare(x,y);
+	printf("min is %d\n", get_min(x,y));
+	printf("max is %d\n", get_max(x,y));
 	return 0;
 }
