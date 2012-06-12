a268a6f00b47258b31d67c7a747b33815a8ebbe3
Teodora Baluta <teobaluta@gmail.com>
2012-06-12 12:41:01 +0300

    Add palindrom functionality.

--- test.c
+++ test.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+/** Testeaza daca numarul x este palindrom
+ * **/
+int palindrom(int x) {
+	int new_x, cifra, y;
+        new_x = x;
+	y = 0;
+	while(!new_x) {
+		cifra = new_x % 10; 
+		y = y*10 + cifra;
+		new_x = new_x/10;
+	}
+	if (y == x) {
+		return 1;
+	}
+	return 0;
+}
+
 void citire(int *x,int *y){
 	scanf("%d%d",x,y);
 }
@@ -12,6 +29,9 @@ int main()
 {
 	int x,y;
 	citire(&x,&y);
+	if (palindrom(x)== 1 && palindrom(y)== 1) {
+		printf("Da\n");
+	}
 	afisare(x,y);
 	return 0;
 }
